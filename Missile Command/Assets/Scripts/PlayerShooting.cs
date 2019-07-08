c94f9fce8dd9b19a395de7f973bb8f9c2e8f2856
Selva_Tekhne <selva.aw@gmail.com>
2019-07-08 16:12:24 +0200

    Create the project, add mouse clicking function.

---
+++ Missile Command/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+	public Vector2 clickPosition;
+	void Update()
+	{
+		
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			clickPosition = Input.mousePosition;
+			Debug.Log("Pressed primary button.");
+			Debug.Log(clickPosition);
+			/*Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray))
+				Instantiate(particle, transform.position, transform.rotation); */
+		}
+	}
+}
