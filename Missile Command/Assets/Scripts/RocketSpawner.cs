c2679c392a7a2d000f48e36a3d7504b38ecb411b
Woltus <woltuszw@gmail.com>
2019-07-15 21:13:51 +0200

    Updating

--- Missile Command/Assets/Scripts/RocketSpawner.cs
+++ Missile Command/Assets/Scripts/RocketSpawner.cs
@@ -5,28 +5,52 @@ using UnityEngine;
 
 public class RocketSpawner : MonoBehaviour
 {
-	public GameObject enmyRocket;
+	public GameObject enemyRocket;
 	public int totalNumberOfRocketsToSpawn = 10;
+    public int maxNumberOfRocketsInWave = 2;
     private int _numberOfRockets = 0;
+    private int _totalNumberOfSpawnedRockets = 0;
+    public float waveDuration = 3;
+    private bool isItAllRockets = false;
 
     public static event Action AllRocketsDestroyed;
+    WaitForSeconds waiting;
 
 	void Start()
 	{
         EnemyRocket.Destroyed += OnRocketDestroyed;
-		for (int i = 0; i < totalNumberOfRocketsToSpawn; i++)
-		{
-			Vector3 dropPosition = new Vector3(UnityEngine.Random.Range(-6f, 6f), 4, 0);
-			Instantiate(enmyRocket, dropPosition, Quaternion.identity);
-            _numberOfRockets++;
-		}
+        waiting = new WaitForSeconds(waveDuration);
 
-	}
+        StartCoroutine(SpawningRockets());
+
+    }
+
+    IEnumerator SpawningRockets()
+    {
+        while (_totalNumberOfSpawnedRockets < totalNumberOfRocketsToSpawn)
+        {
+            WaveSpawning();
+            yield return waiting;
+        }
+        isItAllRockets = true;
+    }
+
+    private void WaveSpawning()
+    {
+        for (int i = 0; i < maxNumberOfRocketsInWave; i++)
+        {
+            //Dodać sprawdzanie czy nie przekroczyliśmy total i randomizowanie liczby ralkiet
+            Vector3 dropPosition = new Vector3(UnityEngine.Random.Range(-6f, 6f), 4, 0);
+            Instantiate(enemyRocket, dropPosition, Quaternion.identity);
+            _numberOfRockets++;
+            _totalNumberOfSpawnedRockets++;
+        }
+    }
 
     private void OnRocketDestroyed()
     {
         _numberOfRockets--;
-        if(_numberOfRockets <= 0)
+        if(_numberOfRockets <= 0 && isItAllRockets)
         {
             AllRocketsDestroyed?.Invoke();
         }
