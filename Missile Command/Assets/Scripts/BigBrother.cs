02b38c169651e7ec317dfc032e8e0e1992ececcf
Woltus <woltuszw@gmail.com>
2019-07-28 14:51:14 +0200

    Transform noise on big brother

--- Missile Command/Assets/Scripts/BigBrother.cs
+++ Missile Command/Assets/Scripts/BigBrother.cs
@@ -6,14 +6,30 @@ using System;
 public class BigBrother : MonoBehaviour, IDestroyable
 {
     public static event Action Destroyed;
-    [SerializeField] private float speed = 1;
+    [SerializeField] private float speed = 1, noiseInfluence = 1;
     public GameObject enemyRocket;
+    private Transform target;
+
+    private float random;
+
+    private void Awake()
+    {
+        UnityEngine.Random.InitState(DateTime.Now.Millisecond);
+        random = UnityEngine.Random.Range(0, 1000);
+        target = FindObjectOfType<PlayerShooting>().transform;
+    }
 
     public void Update()
     {
         //float step = speed * Time.deltaTime;
-        this.transform.position += this.transform.forward * Time.deltaTime * speed;
+        this.transform.position += this.transform.forward * Time.deltaTime * speed + this.transform.right * GetNoise() * Time.deltaTime * noiseInfluence;
+        transform.LookAt(target, this.transform.up);
         //this.transform.position = Vector3.MoveTowards(this.transform.position, targetPosition, step);
+    }
+
+    private float GetNoise()
+    {
+        return Mathf.PerlinNoise(this.transform.position.x * random, this.transform.position.y * random) - 0.5f;
     }
 
     public void Destroy()
